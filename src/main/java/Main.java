ba56a92bd76b91278c945c2f709ad7d63d774c16
Donna Lemon <donnalemon1@gmail.com>
2022-04-20 11:36:54 -0400

    Formatting Numbers.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
 import java.awt.*;
+import java.text.CompactNumberFormat;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-      int result = (int) (Math.random() * 100);
+      // $1,234,567
+        String result = NumberFormat.getPercentInstance().format(0.1);
         System.out.println(result);
-
 
 
     }
