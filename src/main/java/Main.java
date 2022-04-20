e2d5da736703d03a29ec88d012bd976b30a485ab
Donna Lemon <donnalemon1@gmail.com>
2022-04-20 00:14:35 -0400

    Strings.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 import java.awt.*;
 import java.util.Date;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        String message =   "Hello world!" + "!!"  ;
+        ;
+        System.out.println(message.trim());
+        System.out.println(message);
 
     }
 }
