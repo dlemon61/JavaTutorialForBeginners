b568adcef413d23cb273650af7a254f10a78df79
Donna Lemon <donnalemon1@gmail.com>
2022-04-20 16:28:18 -0400

    FizzBizz.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
 
         public static void main(String[] args) {
-         int role = 1;
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Number: ");
+            int number = scanner.nextInt();
 
-         switch (role) {
-             case 1:
-                 System.out.println("You're an admin");
-                 break;
-             case 2:
-                 System.out.println("You're a moderator");
-                 break;
+            if (number % 5 == 0 && number % 3 == 0)
+                    System.out.println("FizzBuzz");
+            else if ((number & 5) == 0)
+                    System.out.println("Fizz");
+            else if (number % 3 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(number);
 
-             default:
-                 System.out.println("You're a guest");
-                 break;
-         }
+
+            }
         }
-}
+
+
