038d0d2a78e773ea44801dbe84c75331755d57d8
Donna Lemon <donnalemon1@gmail.com>
2022-04-19 23:08:34 -0400

    Primitive Types.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int myAge = 61;
-        int hisAge = myAge;
-        System.out.println(hisAge);
+        byte age = 61;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
     }
 }
