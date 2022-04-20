736bdb44f0b8d0ae787e404703ec9d60ed25a33e
Donna Lemon <donnalemon1@gmail.com>
2022-04-20 16:39:38 -0400

    For Loops.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,19 +3,8 @@ import java.util.Scanner;
 public class Main {
 
         public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Number: ");
-            int number = scanner.nextInt();
-
-            if (number % 5 == 0 && number % 3 == 0)
-                    System.out.println("FizzBuzz");
-            else if ((number & 5) == 0)
-                    System.out.println("Fizz");
-            else if (number % 3 == 0)
-                System.out.println("Buzz");
-            else
-                System.out.println(number);
-
+            for (int i = 5; i > 0; i--)
+            System.out.println("Hello world! " + i);
 
             }
         }
