48ab83fb35b1a2b125c5a4ba2b6c1e21753e7a0d
Donna Lemon <donnalemon1@gmail.com>
2022-04-20 15:11:54 -0400

    Simplifying If Statements.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,11 @@
 public class Main {
 
         public static void main(String[] args) {
-          int temp = 32;
-          if (temp > 30); {
-                System.out.println("It's a hot day.");
-                System.out.println("...drink water.");
-            }
-          else if (temp > 20)
-                System.out.println("It's a warm day.");
-          else
-                System.out.println("It's a cold day.");
-            }
+          int income = 120_000;
+          boolean hasHighIncome = (income > 100_000);
+
+
+
 
 
 
