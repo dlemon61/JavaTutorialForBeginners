5903589adf7a5c60f3dece5dac20baeed6446adb
Donna Lemon <donnalemon1@gmail.com>
2022-04-20 14:17:44 -0400

    Mortgage Calculator.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,34 @@
-import java.awt.*;
-import java.sql.SQLOutput;
-import java.text.CompactNumberFormat;
 import java.text.NumberFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Locale;
+import java.time.Month;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Age: ");
-        String name = scanner.next();
-        System.out.println("You are " + name);
+
+        public static void main(String[] args) {
+            final byte Months_IN_YEAR = 12;
+            final byte PERCENT = 100;
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Principal: ");
+            int principal = scanner.nextInt();
+
+            System.out.println("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            float monthlyInterest = annualInterest / PERCENT / Months_IN_YEAR;
+
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            int numberOfPayments = years * Months_IN_YEAR;
+
+            double mortgage = principal
+                    * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments) -1)
+                    / (Math.pow(1 + monthlyInterest, numberOfPayments) -1);
+
+            String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+            System.out.print("Mortgage: " + mortgageFormatted);
 
 
-    }
+
+        }
 }
