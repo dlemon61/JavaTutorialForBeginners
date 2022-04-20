fe8dc6789cb6b4a65558940bf2c43b55e258fbb1
Donna Lemon <donnalemon1@gmail.com>
2022-04-20 14:35:25 -0400

    Conditional Operators.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,10 @@
-import java.text.NumberFormat;
-import java.time.Month;
-import java.util.Scanner;
 
 public class Main {
 
         public static void main(String[] args) {
-            final byte Months_IN_YEAR = 12;
-            final byte PERCENT = 100;
-
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Principal: ");
-            int principal = scanner.nextInt();
-
-            System.out.println("Annual Interest Rate: ");
-            float annualInterest = scanner.nextFloat();
-            float monthlyInterest = annualInterest / PERCENT / Months_IN_YEAR;
-
-            System.out.print("Period (Years): ");
-            byte years = scanner.nextByte();
-            int numberOfPayments = years * Months_IN_YEAR;
-
-            double mortgage = principal
-                    * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments) -1)
-                    / (Math.pow(1 + monthlyInterest, numberOfPayments) -1);
-
-            String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-            System.out.print("Mortgage: " + mortgageFormatted);
-
-
+           int x = 1;
+           int y = 2;
+            System.out.println(x <= y);
 
         }
 }
